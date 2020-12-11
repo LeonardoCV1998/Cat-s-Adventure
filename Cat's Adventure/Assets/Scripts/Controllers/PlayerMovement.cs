1c8c4cd807d869d1b72697818ef8a23e8fa0d108
LeonardoCV1998 <Leocastrovalladolid@hotmail.com>
2020-12-11 01:08:39 -0600

    Implementacion de animaciones
    Se implementaron las animaciones siguientes:
    
    1. Mover de izquierda a derecha
    2. Idle
    3. Jump
    4. Slide
    
    Se necesita que se añada la funcionalidad de evitar el collider de la cabeza para concuerde con la animacion.

--- Cat's Adventure/Assets/Scripts/Controllers/PlayerMovement.cs
+++ Cat's Adventure/Assets/Scripts/Controllers/PlayerMovement.cs
@@ -24,28 +24,48 @@ public class PlayerMovement : MonoBehaviour
     /// </summary>
     private PlayerController _playerController;
 
+    /// <summary>
+    /// Control del animator del player
+    /// </summary>
+    private Animator _animator;
+
 
     void Start()
     {
         // Se obtienen los componente de PlayerController
         _playerController = GetComponent<PlayerController>();
 
-        // En caso de que sea NULL
-        if(_playerController == null)
+        // Se obtienen los componente de Animator
+        _animator = GetComponent<Animator>();
+
+        #region CHECK_COMPONENTS_EXITS
+
+        // Se revisa si los componentes a los cuales se quieren obtener existan, 
+        // si no existen se debugeara un error
+
+        if (_playerController == null)
         {
-            // Se debugueara el error de que no se encontro el componente
-            Debug.Log(StringsType.PlayerControllerIsNull);
+            Debug.LogError(StringsType.PlayerControllerIsNull);
         }
+
+        if(_animator == null)
+        {
+            Debug.LogError(StringsType.AnimatorIsNull);
+        }
+
+        #endregion
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Se manda a llamar en todo momento el metodo de movement 
         // para controlar al player
         Movement();
 
+        #region CHECK_INPUTS
+
         // Chequeo de presion de teclas
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
         }
@@ -63,6 +83,8 @@ public class PlayerMovement : MonoBehaviour
         {
             Attack();
         }
+
+        #endregion
     }
 
     /// <summary>
@@ -76,6 +98,10 @@ public class PlayerMovement : MonoBehaviour
         // por la velocidad que tendra el jugador
         float horizontalInput = Input.GetAxisRaw(StringsType.HorizontalInput) * _runSpeed;
 
+        // Se edita el parametro del animator Speed para controlar la animacion
+        // Se manda el Horizontal Input en Abs para obtener el valor siempre en positivo
+        _animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+
         // Se manda a llamar el metodo de Move y se le pasa la direccion y velocidad
         _playerController.Move(horizontalInput * Time.fixedDeltaTime, false, _jump, _slide);
 
@@ -87,6 +113,7 @@ public class PlayerMovement : MonoBehaviour
     private void Jump()
     {
         _jump = true;
+        _animator.SetBool("IsJumping", true);
     }
 
     /// <summary>
@@ -96,7 +123,10 @@ public class PlayerMovement : MonoBehaviour
     {
         // Se ajusta _slide a true ya que presiono la tecla
         _slide = true;
-        
+
+        // Se envia un true al parametro de IsSlide para la animacion
+        _animator.SetBool("IsSlide", true);
+
         // Se inicia la coroutina para parar el slide
         StartCoroutine(StopSlider());
     }
@@ -112,10 +142,18 @@ public class PlayerMovement : MonoBehaviour
 
         // Se hace falso la variable de slide para que ya no lo haga
         _slide = false;
+
+        // Se envia un false al parametro de IsSlide para la animacion
+        _animator.SetBool("IsSlide", false);
     }
 
     private void Attack()
     {
         Debug.Log("Attacking");
+    }
+
+    public void OnLanding()
+    {
+        _animator.SetBool("IsJumping", false);
     }
 }
